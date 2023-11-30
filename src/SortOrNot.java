844809674505741af71bc94da053d6a92741c294
Somnathrosy <somnathsomnath143143@gmail.com>
2023-11-30 12:52:22 +0530

    New

--- src/SortOrNot.java
+++ src/SortOrNot.java
@@ -8,7 +8,7 @@ public class SortOrNot {
 
     }
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,2,4,5,6,7,8};
+        int[] arr = new int[]{1,2,2,4,5,6,7};
         int n = arr.length;
         System.out.println(Is_Sort(arr,n));
 
